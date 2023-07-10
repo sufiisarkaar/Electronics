f1c630098e4c9e94f746f92751e49f8ea7b4d085
sufiisarkaar <sufiisarkaar@gmail.com>
2023-07-10 17:17:04 +0530

    changes into sql

--- shopsecounter.sql
+++ shopsecounter.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 29, 2023 at 09:13 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Generation Time: Jul 10, 2023 at 01:45 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,376 +26,88 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `addproducts`
 --
-
-CREATE TABLE `addproducts` (
-  `item_id` bigint(20) UNSIGNED NOT NULL,
-  `item_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_category` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_price` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_dsc` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_qty` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_discount` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `item_subtotal` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- Error reading structure for table shopsecounter.addproducts: #1932 - Table &#039;shopsecounter.addproducts&#039; doesn&#039;t exist in engine
+-- Error reading data for table shopsecounter.addproducts: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `shopsecounter`.`addproducts`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `add_to_carts`
 --
-
-CREATE TABLE `add_to_carts` (
-  `cart_id` bigint(20) UNSIGNED NOT NULL,
-  `user_id` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `item_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_category` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_price` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_dsc` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_qty` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_discount` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `item_subtotal` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `add_to_carts`
---
-
-INSERT INTO `add_to_carts` (`cart_id`, `user_id`, `item_id`, `item_name`, `item_category`, `item_price`, `item_dsc`, `item_qty`, `item_image`, `item_discount`, `item_subtotal`, `created_at`, `updated_at`) VALUES
-(1, NULL, '6', 'Girls Sunglasses', 'Sunglasses', '100', 'This Good Quality RayBeen Sunglass.', '1', 'https://pngfre.com/wp-content/uploads/sunglasses.jpg', '11', '89', '2023-05-28 14:25:30', '2023-05-28 14:25:30'),
-(2, NULL, '5', 'Puma Shoes', 'Shoes', '2200', 'This Good Quality Nike Shoes.', '1', 'https://e7.pngegg.com/pngimages/873/234/png-clipart-pair-of-green-and-white-low-top-shoes-puma-shoe-sneakers-nike-footwear-green-running-shoes-orange-fashion.png', '13', '1914', '2023-05-28 14:26:07', '2023-05-28 14:26:07');
+-- Error reading structure for table shopsecounter.add_to_carts: #1932 - Table &#039;shopsecounter.add_to_carts&#039; doesn&#039;t exist in engine
+-- Error reading data for table shopsecounter.add_to_carts: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `shopsecounter`.`add_to_carts`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `failed_jobs`
 --
-
-CREATE TABLE `failed_jobs` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- Error reading structure for table shopsecounter.failed_jobs: #1932 - Table &#039;shopsecounter.failed_jobs&#039; doesn&#039;t exist in engine
+-- Error reading data for table shopsecounter.failed_jobs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `shopsecounter`.`failed_jobs`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `items`
 --
-
-CREATE TABLE `items` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `item_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `item_category` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `item_price` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `item_discount` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `item_image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `item_dsc` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `item_qty` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `items`
---
-
-INSERT INTO `items` (`id`, `item_name`, `item_category`, `item_price`, `item_discount`, `item_image`, `item_dsc`, `item_qty`, `created_at`, `updated_at`) VALUES
-(1, 'Levis Jeans', 'Jeans', '1600', '16', 'https://freepngimg.com/download/jeans/8-2-jeans-png-image.png', 'This Good Quality Levis Jeans.', '1', '2023-05-28 09:18:39', '2023-05-28 09:18:39'),
-(2, 'Addiction Shirts', 'Shirts', '600', '10', 'https://www.pngall.com/wp-content/uploads/2016/04/Dress-Shirt-Free-Download-PNG.png', 'This Good Quality Addiction Shirts.', '1', '2023-05-28 09:39:01', '2023-05-28 09:39:01'),
-(4, 'X-series T-Shirts', 'T-shirts', '200', '18', 'https://png.pngtree.com/png-vector/20201128/ourmid/pngtree-mens-t-shirt-png-image_2400819.jpg', 'This Good Quality Addiction T-Shirts.', '1', '2023-05-28 11:52:59', '2023-05-28 11:52:59'),
-(5, 'Puma Shoes', 'Shoes', '2200', '13', 'https://e7.pngegg.com/pngimages/873/234/png-clipart-pair-of-green-and-white-low-top-shoes-puma-shoe-sneakers-nike-footwear-green-running-shoes-orange-fashion.png', 'This Good Quality Nike Shoes.', '1', '2023-05-28 11:55:50', '2023-05-28 11:55:50'),
-(6, 'Girls Sunglasses', 'Sunglasses', '100', '11', 'https://pngfre.com/wp-content/uploads/sunglasses.jpg', 'This Good Quality RayBeen Sunglass.', '1', '2023-05-28 11:57:23', '2023-05-28 11:57:23');
+-- Error reading structure for table shopsecounter.items: #1932 - Table &#039;shopsecounter.items&#039; doesn&#039;t exist in engine
+-- Error reading data for table shopsecounter.items: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `shopsecounter`.`items`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `migrations`
 --
-
-CREATE TABLE `migrations` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `migrations`
---
-
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
-(3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-(5, '2023_05_05_055724_create_items_table', 1),
-(6, '2023_05_06_071021_create_add_to_carts_table', 1),
-(7, '2023_05_10_061425_create_pending_carts_table', 1),
-(8, '2023_05_15_190440_create_my__orders_table', 1),
-(9, '2023_05_17_190419_create_sellers_table', 1),
-(10, '2023_05_17_195344_create__addproducts_table', 1);
+-- Error reading structure for table shopsecounter.migrations: #1932 - Table &#039;shopsecounter.migrations&#039; doesn&#039;t exist in engine
+-- Error reading data for table shopsecounter.migrations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `shopsecounter`.`migrations`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `my_orders`
 --
-
-CREATE TABLE `my_orders` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `user_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_category` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_price` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_dsc` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_qty` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_discount` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `item_subtotal` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- Error reading structure for table shopsecounter.my_orders: #1932 - Table &#039;shopsecounter.my_orders&#039; doesn&#039;t exist in engine
+-- Error reading data for table shopsecounter.my_orders: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `shopsecounter`.`my_orders`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `password_reset_tokens`
 --
-
-CREATE TABLE `password_reset_tokens` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- Error reading structure for table shopsecounter.password_reset_tokens: #1932 - Table &#039;shopsecounter.password_reset_tokens&#039; doesn&#039;t exist in engine
+-- Error reading data for table shopsecounter.password_reset_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `shopsecounter`.`password_reset_tokens`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `pending_carts`
 --
-
-CREATE TABLE `pending_carts` (
-  `cart_id` bigint(20) UNSIGNED NOT NULL,
-  `user_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_category` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_price` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_dsc` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_qty` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_discount` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `item_subtotal` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- Error reading structure for table shopsecounter.pending_carts: #1932 - Table &#039;shopsecounter.pending_carts&#039; doesn&#039;t exist in engine
+-- Error reading data for table shopsecounter.pending_carts: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `shopsecounter`.`pending_carts`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `personal_access_tokens`
 --
-
-CREATE TABLE `personal_access_tokens` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `last_used_at` timestamp NULL DEFAULT NULL,
-  `expires_at` timestamp NULL DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- Error reading structure for table shopsecounter.personal_access_tokens: #1932 - Table &#039;shopsecounter.personal_access_tokens&#039; doesn&#039;t exist in engine
+-- Error reading data for table shopsecounter.personal_access_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `shopsecounter`.`personal_access_tokens`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `sellers`
 --
-
-CREATE TABLE `sellers` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `phone` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `email_verified_at` timestamp NULL DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- Error reading structure for table shopsecounter.sellers: #1932 - Table &#039;shopsecounter.sellers&#039; doesn&#039;t exist in engine
+-- Error reading data for table shopsecounter.sellers: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `shopsecounter`.`sellers`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
-
-CREATE TABLE `users` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `phone` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `email_verified_at` timestamp NULL DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `addproducts`
---
-ALTER TABLE `addproducts`
-  ADD PRIMARY KEY (`item_id`);
-
---
--- Indexes for table `add_to_carts`
---
-ALTER TABLE `add_to_carts`
-  ADD PRIMARY KEY (`cart_id`);
-
---
--- Indexes for table `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
-
---
--- Indexes for table `items`
---
-ALTER TABLE `items`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `migrations`
---
-ALTER TABLE `migrations`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `my_orders`
---
-ALTER TABLE `my_orders`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `password_reset_tokens`
---
-ALTER TABLE `password_reset_tokens`
-  ADD PRIMARY KEY (`email`);
-
---
--- Indexes for table `pending_carts`
---
-ALTER TABLE `pending_carts`
-  ADD PRIMARY KEY (`cart_id`);
-
---
--- Indexes for table `personal_access_tokens`
---
-ALTER TABLE `personal_access_tokens`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
-  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
-
---
--- Indexes for table `sellers`
---
-ALTER TABLE `sellers`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `sellers_email_unique` (`email`),
-  ADD UNIQUE KEY `sellers_phone_unique` (`phone`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `users_email_unique` (`email`),
-  ADD UNIQUE KEY `users_phone_unique` (`phone`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `addproducts`
---
-ALTER TABLE `addproducts`
-  MODIFY `item_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `add_to_carts`
---
-ALTER TABLE `add_to_carts`
-  MODIFY `cart_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `items`
---
-ALTER TABLE `items`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `migrations`
---
-ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT for table `my_orders`
---
-ALTER TABLE `my_orders`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `pending_carts`
---
-ALTER TABLE `pending_carts`
-  MODIFY `cart_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `personal_access_tokens`
---
-ALTER TABLE `personal_access_tokens`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `sellers`
---
-ALTER TABLE `sellers`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+-- Error reading structure for table shopsecounter.users: #1932 - Table &#039;shopsecounter.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table shopsecounter.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `shopsecounter`.`users`&#039; at line 1
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
